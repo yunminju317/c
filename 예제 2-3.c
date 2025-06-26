4f0dd839af865bcb94a9e9fefe7d01403b3a252e
yunminju317 <ymj1942803@gmail.com>
2025-06-26 10:54:26 +0900

    Add files via upload

---
+++ 예제 2-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>	
+
+int main(void)
+{
+	printf("Be happy\n");
+	printf("12345678901234567890\n");
+	printf("My\tfrined\n");
+	printf("Goot\bd\tchance\n");
+	printf("Cow\rw\a\n");
+
+	return 0;
+}
