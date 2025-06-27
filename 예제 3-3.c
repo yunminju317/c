06981cf1d08a5b9c03ebbcd4c744c811bb8c4af2
yunminju317 <ymj1942803@gmail.com>
2025-06-27 23:44:29 +0900

    Add files via upload

---
+++ 예제 3-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	short sh = 32767;
+	int in = 2147483647;
+	long ln = 2147483647;
+	long long lln = 1234512345;
+
+	printf("short형 변수 출력 : %d\n", sh);
+	printf("int형 변수 출력 : %d\n", in);
+	printf("long형 변수 출력 %ld\n", ln);
+	printf("long long형 변수 출력 : %lld\n", lln);
+
+	return 0;
+}
